133f7fd85dc5bafad0e000b0bbc6ef1570210289
Mari Montero <angelomari.montero@carsu.edu.ph>
2025-10-22 15:34:02 +0800

    Texture path fixes 💀
    Dear god, please someone tell me why `/textures/...` doesn't read, but `textures/...` does 💀

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
+import { defineConfig } from "vite"
+
 const isCodeSandbox = 'SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env
 
-export default {
+export default defineConfig({
     root: 'src/',
     publicDir: '../static/',
-    base: './',
+    base: '/ITE-18-Activity-2.3/',
     server:
     {
         host: true,
@@ -15,4 +17,4 @@ export default {
         emptyOutDir: true,
         sourcemap: true
     }
-}
+})
